ecb5b8eae7ab0925a9796f065b502308432d7b9f
RetaxMaster <retaxmaster@hotmail.com>
2021-01-28 22:04:31 -0600

    Creacion deuna ruta de recursos

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::apiResource("v1/posts", \App\Http\Controllers\Api\V1\PostController::class);
